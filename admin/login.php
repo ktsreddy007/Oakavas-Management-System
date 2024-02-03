c5b75bd883f231ab57ef58acd42c0925adb33d27
ktsreddy007 <cloudtech047@gmail.com>
2024-02-03 19:16:14 +0530

    Error message changed

--- admin/login.php
+++ admin/login.php
@@ -10,7 +10,7 @@
 		$query = $conn->query($sql);
 
 		if($query->num_rows < 1){
-			$_SESSION['error'] = 'Cannot find account with the username';
+			$_SESSION['error'] = 'Cannot find account with the given credentials';
 		}
 		else{
 			$row = $query->fetch_assoc();
