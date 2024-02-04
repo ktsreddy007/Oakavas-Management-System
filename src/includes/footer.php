0d7738cf1bb96beb9af0af0c61db505dd2dc5826
ktsreddy007 <cloudtech047@gmail.com>
2024-02-04 16:48:24 +0530

    Restructed Project

---
+++ src/includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2021 <a href="https://www.alphacodecamp.com.ng">alphacodecamp</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
